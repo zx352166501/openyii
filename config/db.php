2ae9ff54fdb275ff2a5232cf3ddc647e75c02c80
e <e>
2018-01-24 18:37:49 +0800

    autoload fix

--- config/db.php
+++ config/db.php
@@ -1,10 +1,6 @@
 <?php
 
 return [
-//    'dsn' => 'pgsql:host=10.0.21.87;port=5432;dbname=scm',
-//    'username' => 'postgres',
-//    'password' => '123456',
-//    'charset' => 'utf8',
     'dsn' => 'mysql:host=localhost;dbname=school',
     'username' => 'root',
     'password' => 'root',
