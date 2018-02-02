86ca277495c737875e6601ac15eb8e834f114e4f
e <e>
2018-02-02 14:35:06 +0800

    restful Api develope

--- config/urlmanage.php
+++ config/urlmanage.php
@@ -6,10 +6,9 @@
  * Time: 14:04
  */
 return array(
-    'enablePrettyUrl' => true,   // 路由美化
-    'enableStrictParsing' => true,  // 严格检查路由美化,后缀加s
-    'showScriptName' => false,
-    'cache' => false, // 关闭路由缓存
+    'enablePrettyUrl' => true,   // todo 路由美化
+    'enableStrictParsing' => true,  //todo 严格检查路由美化,后缀加s
+    'cache' => false, //todo 关闭路由缓存
     'rules' => [
         //控制器 =》 配置
         'user' => [
